ed888906f2c6008157eb5ad59f83861e6c50ec24
schandreas <andreas@restental.at>
2024-02-28 21:25:11 +0100

    Fix GDS

--- src/tt_um_rgbled_decoder.v
+++ src/tt_um_rgbled_decoder.v
@@ -14,7 +14,7 @@
 
 `default_nettype none
 
-module tt_um_rgbled_decoder #(parameter LEDS = 8, parameter BITS_PER_LED = 24) (
+module tt_um_rgbled_decoder #(parameter LEDS = 3, parameter BITS_PER_LED = 24) (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
@@ -47,5 +47,9 @@ module tt_um_rgbled_decoder #(parameter LEDS = 8, parameter BITS_PER_LED = 24) (
         .sclk(ui_in[1]),
         .nsel(ui_in[2])
         );
+
+    assign uo_out[7:1] = 7'b0;
+    assign uio_oe = 8'b0;
+    assign uio_out = 8'b0;
 
 endmodule
